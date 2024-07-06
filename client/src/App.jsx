c22d474702a7634b43a7c89237a4da94770f39ee
new-username <new-email@example.com>
2024-07-06 11:58:17 +0530

    changes done

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Search from './pages/Search';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    {/* <ScrollToTop /> */}
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/projects' element={<Projects />} />
+      
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/UpdatePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdatePost = () => {
+  return (
+    <div>
+      UpdatePost
+    </div>
+  )
+}
+
+export default UpdatePost
