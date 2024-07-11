6a2f3dc11aa85467e82f6ab42ba97efb42bdd183
Yash Desai <yashd1272@gmail.com>
2024-07-11 18:12:53 +0530

    create redux store for user

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,44 +2,44 @@ import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 //import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  //const { loading, error: errorMessage } = useSelector((state) => state.user);
-  //const dispatch = useDispatch();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // if (!formData.email || !formData.password) {
-    //   return dispatch(signInFailure('Please fill all the fields'));
-    // }
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
+    }
     try {
-      //dispatch(signInStart());
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      // if (data.success === false) {
-      //   dispatch(signInFailure(data.message));
-      // }
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
 
       if (res.ok) {
-        //dispatch(signInSuccess(data));
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      //dispatch(signInFailure(error.message));
+      dispatch(signInFailure(error.message));
       console.log(error.message)
     }
   };
@@ -84,7 +84,7 @@ export default function SignIn() {
             <Button
               gradientDuoTone='purpleToPink'
               type='submit'
-             //disabled={loading}
+             disabled={loading}
             >
              
                 Sign In
@@ -98,11 +98,11 @@ export default function SignIn() {
               Sign Up
             </Link>
           </div>
-          {/* {errorMessage && (
+          {errorMessage && (
             <Alert className='mt-5' color='failure'>
               {errorMessage}
             </Alert>
-          )} */}
+          )}
         </div>
       </div>
     </div>
