4cbb07950b0d168eb0d17ebe9ada2ff0594c422a
Yash Desai <yashd1272@gmail.com>
2024-07-15 13:05:42 +0530

    show the post

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,19 +5,19 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Want to learn more about me
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Checkout my Portfolio
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://my-portfolio-nextjs-yashd07s-projects.vercel.app/" target='_blank' rel='noopener noreferrer'>
+                    Yash Desai
                 </a>
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src="https://www.codecademy.com/resources/blog/wp-content/uploads/2022/12/software-developer-portfolio-1024x476.png" />
         </div>
     </div>
   )
